f0ffe28647b8d82e20cb546bc7bad2c895995bb4
DukovEgor <dkffe2@gmail.com>
2022-05-19 01:23:18 +0300

    Routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFound from '../../pages/404/404';
+import Catalog from '../../pages/catalog/catalog';
 import Main from '../../pages/main/main';
 import Product from '../../pages/product/product';
 import { AppRoutes } from '../../utils/const';
@@ -8,8 +9,10 @@ function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoutes.Root} element={<Main />} />
-        <Route path={AppRoutes.Product} element={<Product />} />
+        <Route element={<Main />}>
+          <Route path={AppRoutes.Root} element={<Catalog />} />
+          <Route path={AppRoutes.Product} element={<Product />} />
+        </Route>
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
