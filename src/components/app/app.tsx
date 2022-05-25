cb37f7dc2757a551cf516af770c715ece1e422d2
DukovEgor <dkffe2@gmail.com>
2022-05-25 20:08:22 +0300

    File Structure

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
-import NotFound from '../../pages/404/404';
-import Catalog from '../../pages/catalog/catalog';
-import LoadingScreen from '../../pages/loading-screen/loading-screen';
-import Main from '../../pages/main/main';
-import Product from '../../pages/product/product';
-import Redirect from '../../pages/redirect/redirect';
+import NotFound from '../404/404';
+import Catalog from '../catalog/catalog';
+import LoadingScreen from '../loading-screen/loading-screen';
+import Main from '../main/main';
+import Product from '../product/product';
+import Redirect from '../redirect/redirect';
 import { AppRoutes } from '../../utils/const';
+import OptionalRoute from '../optional-route/optinal-route';
 
 function App(): JSX.Element {
   const { isDataLoaded } = useAppSelector(({ DATA }) => DATA);
@@ -21,7 +22,14 @@ function App(): JSX.Element {
         <Route path='/' element={<Main />}>
           <Route index element={<Redirect />} />
           <Route path={`${AppRoutes.Catalog}${AppRoutes.Page}:counter`} element={<Catalog />} />
-          <Route path={`${AppRoutes.Product}/:id`} element={<Product />} />
+          <Route
+            path={`${AppRoutes.Product}/:id`}
+            element={
+              <OptionalRoute>
+                <Product />
+              </OptionalRoute>
+            }
+          />
         </Route>
         <Route path='*' element={<NotFound />} />
       </Routes>
--- src/components/optional-route/optinal-route.tsx
+++ src/components/optional-route/optinal-route.tsx
@@ -1,3 +1,15 @@
-export default function OptionalRoute() {
-  return <div></div>;
+import { Navigate, useParams } from 'react-router-dom';
+import { useAppSelector } from '../../hooks';
+
+interface OptionalRouteProps {
+  children: JSX.Element;
+}
+
+export default function OptionalRoute({ children }: OptionalRouteProps) {
+  const { id } = useParams();
+  const pageId = Number(id);
+
+  const { products } = useAppSelector(({ DATA }) => DATA);
+
+  return products.some((product) => product.id === pageId) ? children : <Navigate to={'*'} />;
 }
